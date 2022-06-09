16fc180cb188cd58816eba52de59c53f552bbe96
newpanjing <newpanjing@163.com>
2022-06-09 22:34:10 +0800

    Update docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "153706f10e3c36aec826d293d7f04859",
-"index.html": "21947fa330ff2f5928cef885a4c7da54",
-"/": "21947fa330ff2f5928cef885a4c7da54",
+"index.html": "1df08efd63e34795ce01f49f519394b5",
+"/": "1df08efd63e34795ce01f49f519394b5",
 "main.dart.js": "3a9b9497602d6bab579a65ef2d36dbf9",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
